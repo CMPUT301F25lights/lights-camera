5e0295376e6ebc78ce63b9aed98c06dcd84fd0f9
Aemad Qamar <mqamar1@ualberta.ca>
2025-10-27 15:15:01 -0600

    US 01.05.05 implemented

--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeFragment.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeFragment.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
@@ -12,55 +14,45 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lotterize.Event;
+import com.example.lotterize.databinding.FragmentEventsRegisteredBinding;
 import com.example.lotterize.databinding.FragmentHomeBinding;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
     private FirebaseFirestore db;
     private CollectionReference events;
     private FragmentHomeBinding binding;
-    private ArrayList<Event> eventList;
+    private ArrayList<DocumentSnapshot> eventList;
+    private ImageButton info;
+
+    private TextView info_text;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        eventList = new ArrayList<>();
         db = FirebaseFirestore.getInstance();
         events = db.collection("events");
+        events.orderBy("date", Query.Direction.DESCENDING).limit(10).get().addOnSuccessListener(snapshot -> {
+            eventList.addAll(snapshot.getDocuments());
+        });
 
-        eventList = new ArrayList<>();
-//        events.addSnapshotListener((value,error) ->{
-//            if (error != null){
-//                Log.e("Firestore", error.toString());
-//            }
-//            if (value != null && !value.isEmpty()){
-//                eventList.clear();
-//                for (QueryDocumentSnapshot snapshot: value){
-//                    User owner = snapshot.getString("eventOwner");
-//                    String eventName = snapshot.getString("eventName");
-//                    String date = snapshot.getString("date");
-//                    long totalSpots = snapshot.getLong("totalSpots") != null ? snapshot.getLong("totalSpots") : 0L;
-//                    String location = snapshot.getString("location");
-//                    long waitListLength = snapshot.getLong("waitListLength") != null ? snapshot.getLong("waitListLength") : 0L;
-//                    String description = snapshot.getString("description");
-//                    long entrantsLimit = snapshot.getLong("entrantsLimit") != null ? snapshot.getLong("entrantsLimit") : 0L;
-//                    long sampleSize = snapshot.getLong("sampleSize") != null ? snapshot.getLong("sampleSize") : 0L;
-//                    ArrayList<String> waitList = new ArrayList<>();// Figure out how to do waitlist
-//
-//                    eventList.add(new Event(eventOwner, eventName,date,location,totalSpots,waitListLength,description,entrantsLimit,sampleSize,waitList));
-//                }
-//            }
-//        });
+        info = binding.infoButton;
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InfoFragment i = new InfoFragment();
+                i.show(getActivity().getSupportFragmentManager(), "Info");
+            }
+        });
 
         return root;
     }
--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeViewModel.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeViewModel.java
@@ -1,19 +1,36 @@
 package com.example.lotterize.ui.home;
 
+import android.text.Html;
+import android.text.Spanned;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<Spanned> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue(Html.fromHtml(
+        "<b><u>How to get selected for events:</u></b> <br>" +
+        "1. Click on the event. <br>" +
+        "2. Click join waiting list. (Make sure registration deadline hasn't already passed!) <br>" +
+        "3. Wait. (You will get a notification telling you if you are chosen or not) <br>" +
+        "4. Once chosen go to 'Events Registered' (2nd from the right in the bottom bar) <br>" +
+        "5. Accept the invitation <br>" +
+        "6. Congragulations! You are now officially attending the event! <br>" +
+        " <br>"+
+        "<b><u>How participants are chosen:</u></b> <br>" +
+        "    Each event has a total number of available position (n) that can be filled. When the organizer clicks the button, " +
+        "the system sends n invitations randomly to people on the waiting list. If anyone on the waiting list declines the " +
+        "invitation, the system sends another random person on the waiting list a invitation. This is repeated until the total " +
+        "number of available positions are filled. Good Luck!"
+        ,Html.FROM_HTML_MODE_LEGACY));
     }
 
-    public LiveData<String> getText() {
+    public LiveData<Spanned> getText() {
         return mText;
     }
 }
