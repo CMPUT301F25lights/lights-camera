352bff1e677e615c0c2b956a232ca82549eff6f3
rajuthekaju17 <144866545+rajuthekaju17@users.noreply.github.com>
2025-11-01 20:59:37 -0600

    commiting to merge

--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/profile/EventHistoryFragment.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/profile/EventHistoryFragment.java
@@ -4,25 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.lotterize.CurrentUser;
 import com.example.lotterize.R;
+import com.example.lotterize.User;
 import com.example.lotterize.databinding.FragmentEventHistoryBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.List;
 
 public class EventHistoryFragment extends Fragment {
 
     private FragmentEventHistoryBinding binding;
+    private FirebaseFirestore db;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentEventHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        db = FirebaseFirestore.getInstance();
 
         if (getActivity() != null) {
             BottomNavigationView navView = getActivity().findViewById(R.id.nav_view);
@@ -38,6 +49,7 @@ public class EventHistoryFragment extends Fragment {
         });
 
         return root;
+
     }
 
     @Override
