dafa26cd4487f036efdfd1e231b1dbbabaa3130c
Aemad Qamar <mqamar1@ualberta.ca>
2025-11-02 18:33:46 -0700

    fixed UI stuff

--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeFragment.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/home/HomeFragment.java
@@ -12,12 +12,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.lotterize.CurrentUser;
+import com.example.lotterize.R;
 import com.example.lotterize.databinding.FragmentHomeBinding;
 
 import com.google.android.material.button.MaterialButton;
@@ -29,6 +33,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     private FirebaseFirestore db;
@@ -41,6 +46,7 @@ public class HomeFragment extends Fragment {
     private ListView eventsListView;
     private ArrayAdapter<DocumentSnapshot> eventsArray;
     private TextView info_text;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -65,6 +71,21 @@ public class HomeFragment extends Fragment {
             eventsArray.notifyDataSetChanged();
         });
 
+        events.orderBy("registrationDeadline").whereGreaterThan("registrationDeadline", Timestamp.now())
+                .addSnapshotListener((snapshot,e) -> {
+                    if (e != null){
+                        Toast.makeText(getContext(), "couldn't update data - HomeFragment", Toast.LENGTH_SHORT).show();
+                    } else {
+                        eventList.clear();
+                        eventList.addAll(snapshot.getDocuments());
+                        shownList.clear();
+                        shownList.addAll(eventList);
+                        eventsArray.notifyDataSetChanged();
+                    }
+
+                    shownList.addAll(eventList);
+                });
+
         info = binding.infoButton;
         eventsListView = binding.eventsList;
         TextInputEditText searchBar = binding.searchBar;
@@ -72,11 +93,25 @@ public class HomeFragment extends Fragment {
         eventsListView.setAdapter(eventsArray);
 
         MaterialButton waitListedEvents = binding.waitlistedEventsButton;
+        String currUserId = CurrentUser.get().getUserId();
         waitListedEvents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(),UserWaitListedEvents.class);
-                getContext().startActivity(intent);
+                v.setSelected(!v.isSelected());
+                if (!v.isSelected()) {
+                    shownList.clear();
+                    shownList.addAll(eventList);
+                    eventsArray.notifyDataSetChanged();
+                } else {
+                    shownList.clear();
+                    for (DocumentSnapshot d : eventList){
+                        List<String> l = (List<String>) d.get("waitList");
+                        if (l != null && l.contains(currUserId)){
+                            shownList.add(d);
+                        }
+                    }
+                    eventsArray.notifyDataSetChanged();
+                }
             }
         });
 
