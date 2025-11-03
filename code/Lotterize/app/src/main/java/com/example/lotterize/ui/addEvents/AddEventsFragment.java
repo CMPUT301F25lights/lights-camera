13b98fab0c5e0f2e4156ebf0ccc28d524a0aadc5
rajuthekaju17 <144866545+rajuthekaju17@users.noreply.github.com>
2025-11-02 18:31:51 -0700

    misc

--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/addEvents/AddEventsFragment.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/addEvents/AddEventsFragment.java
@@ -26,8 +26,6 @@ public class AddEventsFragment extends Fragment {
         binding = FragmentAddEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textAddEvents;
-        addEventsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         binding.buttonNewEvent.setOnClickListener(v -> {
             NavHostFragment.findNavController(AddEventsFragment.this)
                     .navigate(R.id.navigation_addEvents);
