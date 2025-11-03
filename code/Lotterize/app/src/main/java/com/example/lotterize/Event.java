3add5df0a499eb7fac6bbadaaad267aa852ea009
Aemad Qamar <mqamar1@ualberta.ca>
2025-11-02 19:01:28 -0700

    added documentaiton to User, Notification, Event

--- code/Lotterize/app/src/main/java/com/example/lotterize/Event.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/Event.java
@@ -4,6 +4,9 @@ package com.example.lotterize;
 import com.google.firebase.Timestamp;
 import java.util.ArrayList;
 
+/**
+ * Model of Events. Stores all of the information related to events.
+ */
 public class Event {
 
     private String eventId;
@@ -45,62 +48,107 @@ public class Event {
         this.qrCode = qrCode;
     }
 
+    /**
+     * @return String - id of event
+     */
     public String getEventId() {
         return eventId;
     }
 
+    /**
+     * @return String - id of event owner
+     */
     public String getOwnerId() {
         return ownerId;
     }
 
+    /**
+     * @return ArrayList<String>- list containing ids of all users in waiting list
+     */
     public ArrayList<String> getWaitList() {
         return waitList;
     }
 
+    /**
+     * @return ArrayList<String>- list containing ids of all users in selected list
+     */
     public ArrayList<String> getSelectedList() {
         return selectedList;
     }
 
+    /**
+     * @return ArrayList<String>- list containing ids of all users in cancelled list
+     */
     public ArrayList<String> getCancelledList() {
         return cancelledList;
     }
 
+    /**
+     * @return ArrayList<String>- list containing ids of all users in final list
+     */
     public ArrayList<String> getFinalList() {
         return finalList;
     }
 
+    /**
+     * @return String- name of event
+     */
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * @return Timestamp- date of the event
+     */
     public Timestamp getDate(){
         return date;
     }
 
+    /**
+     * @return Timestamp- registration start time
+     */
     public Timestamp getRegistrationStart() {
         return registrationStart;
     }
 
+    /**
+     * @return Timestamp- registration dead line
+     */
     public Timestamp getRegistrationDeadline() {
         return registrationDeadline;
     }
 
+    /**
+     * @return String- location of event
+     */
     public String getLocation(){
         return location;
     }
 
+    /**
+     * @return long- total available spots
+     */
     public long getTotalSpots() {
         return totalSpots;
     }
 
+    /**
+     * @return String- event description
+     */
     public String getDescription(){
         return description;
     }
 
+    /**
+     * @return long- maximum number of entrants
+     */
     public long getEntrantsLimit(){
         return entrantsLimit;
     }
 
+    /**
+     * @return String- qrCode in string form
+     */
     public String getQrCode() {
         return qrCode;
     }
--- code/Lotterize/app/src/main/java/com/example/lotterize/User.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/User.java
@@ -1,5 +1,8 @@
 package com.example.lotterize;
 
+/**
+ * Models a User. Contains all attributes of a user.
+ */
 public class User {
 
     private String name;
@@ -26,33 +29,58 @@ public class User {
         this.password = password;
     }
 
+    /**
+     * @return String - name of user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return String - phone number of user
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * @return String - email of user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * @return String - coordinates of user
+     */
     public String getCoordinates() {
         return coordinates;
     }
 
+    /**
+     * @return String - id of user
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * @return String - username of user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * @return String - password of user
+     */
     public String getPassword() {
         return password;
     }
+
+    /**
+     * @param userId - new id for the user
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
