512ff3ed02c09c1d9b5cc9a46eaa203d725a8457
Minh Bui <nhatbuiminh.1994@gmail.com>
2025-11-03 17:23:48 -0700

    Updated Notificaiton class

--- code/Lotterize/app/src/main/java/com/example/lotterize/Notification.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/Notification.java
@@ -4,12 +4,10 @@ import com.google.firebase.Timestamp;
 
 import java.util.ArrayList;
 
-import kotlin.contracts.Returns;
-
 /**
- * This is a class that defines a Notification.
- * It contains all information related to a notification.
+ * Model class for Notifications. Contains all data related to notifications.
  */
+
 public class Notification {
 
     private String notificationId;
