28d6d9a7fe784c477777d120ae81644cea6ab148
Victor Kwok <tinhangv@ualberta.ca>
2025-11-26 11:32:54 -0700

    map activity javadoc

--- code/Lotterize/app/src/main/java/com/example/lotterize/ui/addEvents/MapsActivity.java
+++ code/Lotterize/app/src/main/java/com/example/lotterize/ui/addEvents/MapsActivity.java
@@ -21,6 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Activity for displaying a Google map of locations where users joined waitlist from.
+ */
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -46,8 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
